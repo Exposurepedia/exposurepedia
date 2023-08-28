518b87272e4a9cda197b626e3f29e3bd001d9af8
Sarah Schwartz <seschwartz8@gmail.com>
2023-08-27 20:38:32 -0500

    turn off requirement for react in scope preventing use of auto-remove of unused imports

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['airbnb-typescript-prettier'],
   root: true,
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'import/prefer-default-export': 0,
     'react/require-default-props': 0,
     'no-console': 'warn',
